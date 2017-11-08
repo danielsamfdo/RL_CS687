8e1ccce146cc176b0d3048865ab60de416206d03
Daniel Sam Pete Thiyagu <danielsamfdo@gmail.com>
2017-11-08 14:41:37 -0500

    Adding Change

--- HW3/libutils.py
+++ HW3/libutils.py
@@ -3,10 +3,10 @@ import numpy as np
 from Qlearning import *
 from env import *
 def hyperparamtuning(ql):
-  alpha = [0.001, 0.01, 0.1, 0.5]
-  gamma = [0.1, 0.5, 0.8, 0.9, 1]
-  epsilon = [0,0.1,0.3,0.4]
-  order = [1,2]
+  alpha = np.logspace(-5, -2, num=1)
+  gamma = [1]#[0.1, 0.5, 0.8, 0.9, 1]
+  epsilon = [0.1]#[0,0.1,0.3,0.4]
+  order = [2]
   params = [(v1,v2,v3,v4) for v1 in alpha for v2 in gamma for v3 in epsilon for v4 in order]
   trials = 2
   t = np.zeros(200)
@@ -15,11 +15,12 @@ def hyperparamtuning(ql):
     t = np.zeros(60)
   for p in params:
       a,g,e,o = p
+      print p
       for i in range(trials):
           l = None
           if(ql):
             env = MountainCar()
-            l = Qlearning(env,order=o, alpha=a, gamma=g, epsilon=e)
+            l = QLearning(env,order=o, alpha=a, gamma=g, epsilon=e)
             l.run(env)
           if(p in testing):
             testing[p] += l.episodes()
@@ -27,3 +28,5 @@ def hyperparamtuning(ql):
             testing[p] = l.episodes()
       testing[p] = testing[p]/trials
   joblib.dump(testing,"hyptest.jobpkl")
+
+hyperparamtuning(True)
