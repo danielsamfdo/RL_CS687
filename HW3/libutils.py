13dcc0176ffcef57104d1049e3a7d8155a8fe97d
Daniel Sam Pete Thiyagu <danielsamfdo@gmail.com>
2017-11-08 15:08:51 -0500

    Adding Sarsa Hyperparam optim

--- HW3/libutils.py
+++ HW3/libutils.py
@@ -1,11 +1,12 @@
 import joblib
 import numpy as np
 from Qlearning import *
+from sarsa import *
 from env import *
 def hyperparamtuning(ql):
-  alpha = np.logspace(-3, -2, num=2)
+  alpha = np.logspace(-2, -2, num=1)
   gamma = [1]#[0.1, 0.5, 0.8, 0.9, 1]
-  epsilon = [0.1,0.01]#[0,0.1,0.3,0.4]
+  epsilon = [0.1]#[0,0.1,0.3,0.4]
   order = [2]
   params = [(v1,v2,v3,v4) for v1 in alpha for v2 in gamma for v3 in epsilon for v4 in order]
   trials = 2
@@ -18,15 +19,21 @@ def hyperparamtuning(ql):
       print p
       for i in range(trials):
           l = None
+          env = MountainCar()
           if(ql):
-            env = MountainCar()
             l = QLearning(env,order=o, alpha=a, gamma=g, epsilon=e)
-            l.run(env)
+          else:
+            l = SARSA(env,order=o, alpha=a, gamma=g, epsilon=e)            
+          l.run(env)
           if(p in testing):
             testing[p] += l.episodes
           else:
             testing[p] = l.episodes
       testing[p] = testing[p]/trials
-  joblib.dump(testing,"hyptest"+str(o)+".jobpkl")
+  prefix = "sarsa"
+  if(ql):
+    prefix = "ql"
+  joblib.dump(testing, prefix +"hyptest_"+str(o)+".jobpkl")
 
+hyperparamtuning(False)
 hyperparamtuning(True)
