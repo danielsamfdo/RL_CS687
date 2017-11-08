dc3183926ec4ced5441772cf5444efabd6815374
Ubuntu <ubuntu@ip-172-31-3-230.us-west-2.compute.internal>
2017-11-08 19:58:55 +0000

    Adding Tweaks

--- HW3/libutils.py
+++ HW3/libutils.py
@@ -3,9 +3,9 @@ import numpy as np
 from Qlearning import *
 from env import *
 def hyperparamtuning(ql):
-  alpha = np.logspace(-5, -2, num=1)
+  alpha = np.logspace(-3, -2, num=2)
   gamma = [1]#[0.1, 0.5, 0.8, 0.9, 1]
-  epsilon = [0.1]#[0,0.1,0.3,0.4]
+  epsilon = [0.1,0.01]#[0,0.1,0.3,0.4]
   order = [2]
   params = [(v1,v2,v3,v4) for v1 in alpha for v2 in gamma for v3 in epsilon for v4 in order]
   trials = 2
@@ -23,10 +23,10 @@ def hyperparamtuning(ql):
             l = QLearning(env,order=o, alpha=a, gamma=g, epsilon=e)
             l.run(env)
           if(p in testing):
-            testing[p] += l.episodes()
+            testing[p] += l.episodes
           else:
-            testing[p] = l.episodes()
+            testing[p] = l.episodes
       testing[p] = testing[p]/trials
-  joblib.dump(testing,"hyptest.jobpkl")
+  joblib.dump(testing,"hyptest"+str(o)+".jobpkl")
 
 hyperparamtuning(True)
